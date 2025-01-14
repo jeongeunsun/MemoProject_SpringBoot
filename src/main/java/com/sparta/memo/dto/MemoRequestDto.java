b597fdee7c5fce224e7b48fca107e1dacb7cfda6
dmstjs915 <dmstjs915@gmail.com>
2025-01-14 21:51:58 +0900

    JDBC

--- src/main/java/com/sparta/memo/dto/MemoRequestDto.java
+++ src/main/java/com/sparta/memo/dto/MemoRequestDto.java
@@ -1,8 +1,10 @@
 package com.sparta.memo.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class MemoRequestDto {
     private String username;
     private String contents;
